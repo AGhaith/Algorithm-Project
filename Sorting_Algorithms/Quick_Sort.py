68a0c853d7325bd4c829c5ff9eef1b5e36df3bc2
omar emad <omaremad11113@gmail.com>
2025-05-02 05:53:55 +0300

    Quick_Sort__omar

--- Sorting_Algorithms/Quick_Sort.py
+++ Sorting_Algorithms/Quick_Sort.py
@@ -1,1 +1,22 @@
-#Pending
+#omda
+import random
+
+def quick_sort(arr):
+
+    if len(arr) <= 1:
+        return arr
+    
+    mid = len(arr) // 2
+    pivot = arr[mid]
+
+    arr = arr[:mid] + arr[mid+1:]
+
+    left = [x for x in arr if x < pivot]
+    right = [x for x in arr if x >= pivot]
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+# arr = quick_sort([3, 6, 8, 10, 1, 2, 1])
+big_arr = [random.randint(0, 1_000_000) for _ in range(100_000)]
+arr = quick_sort(big_arr)
+print(arr)
