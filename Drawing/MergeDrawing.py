afbb684ab38c0678facbd257bfa48d28d7932bf0
omar emad <omaremad11113@gmail.com>
2025-05-10 02:31:55 +0300

    womp womp

--- Drawing/MergeDrawing.py
+++ Drawing/MergeDrawing.py
@@ -1,27 +1,23 @@
 import Utilities.Global_Variables as gv
-
-def MergeDrawing(array ,level , color_array=None):
-
+import tkinter as tk 
+def MergeDrawing(array, level, x_start):
     box_width = 50
     box_height = 30
     box_offset = 10
-    arr = array
-    Merge_Level_Offset = level * 50 
+    vertical_offset = 50
 
-    total_width = len(arr) * (box_width + box_offset) - box_offset
-    start_x = (gv.CANVAS_WIDTH - total_width) / 2
+    
+    start_x = x_start * (box_width + box_offset + 50) + 100
+    y0 = level * vertical_offset
 
-    for i, val in enumerate(arr):
-        x0 = start_x + i * (box_width + box_offset)
-        y0 = 20 + Merge_Level_Offset
-        x1 = x0 + box_width
+    for i, val in enumerate(array):
+        x0 = start_x + i * (box_width + box_offset) + 50 
         y1 = y0 + box_height
+        x1 = x0 + box_width 
 
-        color = color_array[i] if color_array is not None else "lightblue"
-
-        gv.canvas.create_rectangle(x0, y0, x1, y1, fill=color, outline="black")
+        gv.canvas.create_rectangle(x0, y0, x1, y1, fill="lightblue", outline="black")
         gv.canvas.create_text((x0 + x1) / 2, (y0 + y1) / 2, text=str(val), anchor="center", font=("Arial", 10))
 
-    gv.time.sleep(1)
     gv.canvas.update_idletasks()
-    gv.canvas.after(50)
+    gv.time.sleep(0.5)
+
